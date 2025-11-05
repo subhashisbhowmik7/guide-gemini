51a216ec6a93b50b13b5f182c4df63055ad01f09
subhashisbhowmik7 <subhashisbhowmik325@gmail.com>
2025-11-05 20:09:45 +0530

    clinet id chnaged

--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -3,7 +3,7 @@ import { Configuration } from "@azure/msal-browser";
 // Define the MSAL configuration dynamically
 export const msalConfig: Configuration = {
   auth: {
-    clientId: "6cd1e233-be7f-4a37-97cb-08590c2b66d5",
+    clientId: "2689fe12-043c-4471-8891-0acb85e442c6",
     authority: "https://login.microsoftonline.com/1357b1b7-349f-4b78-a7b9-ed814005863d",  //hardcoded
     // Use the dynamic origin of the current running application
     redirectUri: window.location.origin, 
